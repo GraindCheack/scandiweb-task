d4b7de2292e1deabc1b691686ea02f4361634900
Nikon <graindcheack@gmail.com>
2020-10-15 23:18:27 +0300

    feat: add sliders content

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Slider from "./slider/Slider";
+import Slider1Content from '../content/Slider1Content';
+import Slider2Content from '../content/Slider2Content';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Slider />
-        <Slider />
+        <Slider sliderContent={Slider1Content}/>
+        <Slider sliderContent={Slider2Content}/>
       </div>
     );
   }
