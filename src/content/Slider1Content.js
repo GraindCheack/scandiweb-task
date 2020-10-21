d41e6511dbfa540a3ea872c3716ef505f69e82f2
Nikon <graindcheack@gmail.com>
2020-10-21 12:45:48 +0300

    fix: change sliders content

--- src/content/Slider1Content.js
+++ src/content/Slider1Content.js
@@ -1,37 +1,76 @@
 export default [
-  `
-  <div style="text-align: center; width: 100%; height: 100%; background: #F3F3F3; color: #2F3134; font-size: 3rem;">
-    <h1 style="margin: 0;">Hello World!!!</h1>
-    <p>My first slide</p>
-  </div>
-  `,
-  
-  `
-  <div style="text-align: center; background: #2F3134; color: white;">
-    <h2 style="margin: 0; padding: 20px;">Slide-2</h2>
-    <img src="https://i.artfile.ru/3003x2000_894702_%5Bwww.ArtFile.ru%5D.jpg" alt="Nature" style="height: 100%; width: 100%; object-fit: cover">
-  </div>
-  `,
-  `
-  <h1 style="margin: 0; text-align: center;">My form<h1>
-  <form action="#" style="background: #2F3134; display: flex; width: 100%; height: 100px; text-align: center; justify-content: center; align-items: center;">
-    <input type="email" placeholder="Email" style="background: #F3F3F3; outline: none; border: none; border-right: 1px solid rgba(0,0,0,0.2); padding: 4px 12px;">
-    <button type="submit" style="display: block; outline: none; border: none; padding: 4px 3px;">Next</button>
-  </form>
-  `,
-  `
-  <div style="text-align: center; width: 100%; height: 100%; background: #F3F3F3; color: #2F3134; font-size: 3rem;">
-    <h1 style="margin: 0;">Slide-4</h1>
-    <ul style="width: 20%; margin: 0 auto; text-align: left;">
-      <li>First li</li>
-      <li>Second li</li>
-    </ul>
-  </div>
-  `,
-  `
-  <div style="text-align: center; width: 100%; height: 100%; background: #F3F3F3; color: #2F3134; font-size: 3rem;">
-    <h1 style="margin: 0;">Slide-5</h1>
-    <p>The end...</p>
-  </div>
-  `,
+  {
+    tag: 'div',
+    properties: {style: {textAlign: 'center', width: '100%', height: '100%', background: '#F3F3F3', color: '#2F3134', fontSize: '3rem'}},
+    children: [
+      {tag: 'h1', properties: {style: {margin: 0}}, children: 'Hello world!!!'},
+      {tag: 'p', children: 'My first slide'}
+    ]
+  },
+  {
+    tag: 'div',
+    properties: {style: {textAlign: 'center', background: '#2F3134', color: 'white'}},
+    children: [
+      {tag: 'h2', properties: {style: {margin: 0, padding: '20px'}}, children: 'Slide-2'},
+      {
+        tag: 'img',
+        properties: {
+          alt: 'Nature',
+          src: 'https://i.artfile.ru/3003x2000_894702_%5Bwww.ArtFile.ru%5D.jpg',
+          style: {height: '100%', width: '100%', objectFit: 'cover'}
+        },
+      }
+    ]
+  },
+  {
+    tag: 'div',
+    properties: {style: {width: '100%', height: '100%'}},
+    children: [
+      {tag: 'h1', properties: {style: {margin: 0, textAlign: 'center'}}, children: 'My form'},
+      {
+        tag: 'form', 
+        properties: {
+          action: '#',
+          style: {background: '#2F3134', display: 'flex', width: '100%', height: '100px', textAlign: 'center', justifyContent: 'center', alignItems: 'center'}
+        },
+        children: [
+          {
+            tag: 'input', 
+            properties: {
+              type: "email",
+              placeholder: "Email",
+              style: {background: '#F3F3F3', outline: 'none', border: 'none', borderRight: '1px solid rgba(0,0,0,0.2)', padding: '4px 12px'}
+            }
+          },
+          {
+            tag: 'button',
+            properties: {
+              type: 'submit',
+              style: {display: 'block', outline: 'none', border: 'none', padding: '4px 3px'}
+            },
+            children: "Next"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    tag: 'div',
+    properties: {style: {textAlign: 'center', width: '100%', height: '100%', background: '#F3F3F3', color: '#2F3134', fontSize: '3rem'}},
+    children: [
+      {tag: 'h1', properties: {style: {margin: 0}}, children: 'Slide-4'},
+      {tag: 'ul', properties: {style: {width: '20%', margin: '0 auto', textAlign: 'left'}}, children: [
+        {tag: 'li', children: 'First li'},
+        {tag: 'li', children: 'Second li'},
+      ]}
+    ]
+  },
+  {
+    tag: 'div',
+    properties: {style: {textAlign: 'center', width: '100%', height: '100%', background: '#F3F3F3', color: '#2F3134', fontSize: '3rem'}},
+    children: [
+      {tag: 'h1', properties: {style: {margin: 0}}, children: 'Slide-5'},
+      {tag: 'p', children: 'The End...'}
+    ]
+  },
 ]
